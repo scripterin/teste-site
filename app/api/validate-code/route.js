fd99ac752155b62063f252e1216db05719282dab
scripterin <bogdanro777@gmail.com>
2026-04-04 09:29:18 +0300

    Update route.js

--- app/api/validate-code/route.js
+++ app/api/validate-code/route.js
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/lib/authOptions';
 import { NextResponse } from 'next/server';
 import connectDB from '@/lib/mongodb';
 import Code from '@/models/Code';
